d0f8eeb1b6d1e7e55c2cd6646fe2d317c58a43b7
malarc01 <45378877+malarc01@users.noreply.github.com>
2020-01-08 08:47:05 -0500

    added pictures to House component

---
+++ pages/index.js
@@ -0,0 +1,32 @@
+import houses from './houses.json'
+
+import House from '../components/House';
+
+
+
+const Index = () => (
+    <div>
+        <h2>Places to stay</h2>
+        <div className="houses">
+            {houses.map((house,index)=>{
+                return <House key={index}{...house}/>
+            })}
+        </div>
+
+
+
+        <style jsx>
+            {`.houses {
+                display:grid;
+                grid-template-columns: 50% 50%;
+                grid-template-rows: 300px 300px;
+                grid-gap: 40px;
+            }`}
+        </style>
+
+
+
+    </div>
+)
+
+export default Index
