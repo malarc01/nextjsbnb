6e5a779f38852e1f2f716a29b43f7d94ce8d658a
malarc01 <45378877+malarc01@users.noreply.github.com>
2020-02-08 13:55:51 -0500

    imported Header to site index

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "cced5c73a13d1860f421";
+/******/ 	var hotCurrentHash = "9cef5c9814c4e0728864";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
