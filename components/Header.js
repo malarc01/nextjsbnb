ecc6954204759a61aeb1397825430585671bffcf
malarc01 <45378877+malarc01@users.noreply.github.com>
2020-02-08 13:53:20 -0500

    added header styling but not showing up?

--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,44 @@
+import Link from 'next/link'
+
+
 const Header = () => (
     <div className='nav-container'>
-        <img src='/img/logo.png' alt='' />
+        <Link href='/'>
+            <a><img src='/img/logo.png' alt='' /></a>
+        </Link>
         <nav>
-
+            <ul>
+                <li>
+                    <Link href='/register'>
+                        <a>Sign Up</a>
+                    </Link>
+                </li>
+                <li>
+                    <Link href='/login'>
+                        <a>Log in</a>
+                    </Link>
+                </li>
+            </ul>
         </nav>
         <style jsx>{`
+        ul{
+            margin:0;
+            padding:0;
+
+        }
+        li{
+            display:block;
+            float:left;
+        }
+        a{
+            text-decoration:none;
+            display:block;
+            margin-right:15px;
+            color:#333
+        }
+        nav a {
+            padding: 1em 0.5em;
+        }
         .nav-container{
             border-bottom:1px solid #eee;
             height:50px;
@@ -12,6 +46,10 @@ const Header = () => (
         img{
             float:left;
         }
+        ul{
+            float:right;
+        }
+
         `}
         </style>
     </div>
