eeeb9766eee5a2c1c5eaa77920443fb4f771e981
malarc <email@email.com>
2020-06-02 10:33:43 -0400

    add '/api/houses/reserve endpoint

--- server.js
+++ server.js
@@ -230,6 +230,23 @@ nextApp.prepare().then(() => {
 		});
 	});
 
+	server.post('/api/houses/reserve', (req, res) => {
+		const userEmail = req.session.passport.user;
+		User.findOne({ where: { email: userEmail } }).then((user) => {
+			Booking.create({
+				houseId: req.body.houseId,
+				userId: user.id,
+				startDate: req.body.startDate,
+				endDate: req.body.endDate
+			}).then(() => {
+				res.writeHead(200, {
+					'Content-Type': 'application/json'
+				});
+				res.end(JSON.stringify({ status: 'success', message: 'ok' }));
+			});
+		});
+	});
+
 	server.all('*', (req, res) => {
 		return handle(req, res);
 	});
